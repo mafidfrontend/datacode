e12e3c8bf8b4ee503ef8c91db00ff06f358fd45e
mafidfrontend <neo.uz001@gmail.com>
2025-05-28 11:23:32 +0500

    teacherDetails and components added in pages, navbar created C: Abdulloh

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
+import HomePage from "./api/home/page";
+import Navbar from "./components/Navbar";
+
 export default function Home() {
   return (
-    <></>
+    <>
+      <Navbar />
+      <HomePage />
+    </>
   );
 }
